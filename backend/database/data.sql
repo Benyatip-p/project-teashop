85da9a93b25bfecbb1238f18d709e86e9babb672
Benyatip-ongj <phonsantia_b@silpakorn.edu>
2025-10-24 13:24:16 +0000

    init

---
+++ backend/database/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (name, first_name, last_name, email, password_hash, role)
+VALUES (
+  'benyathip',
+  'เบญญทิพย์',
+  'พรสันเทียะ',
+  'benyathip@example.com',
+  '$2b$10$V8bJYzjEZo4gLdRZrGdVnO1vSGs8i6kFxm9ktuzHgEi3vZojh4mwy',
+  'user'
+);
